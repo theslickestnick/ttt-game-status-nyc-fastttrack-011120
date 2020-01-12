959b13fe21c90f1bafa443cd23c3c5625027d04d
Nicholas Echevarria <nick.echev@gmail.com>
2020-01-12 18:56:58 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -11,20 +11,20 @@ WIN_COMBINATIONS = [
   win_combo8 = [2,5,8]
 ]
 
-# win_index_1 = win_combo1[0]
-# win_index_2 = win_combo2[1]
-# win_index_3 = win_combo3[2]
-# win_index_4 = win_combo4[3]
-# win_index_5 = win_combo5[4]
-# win_index_6 = win_combo6[5]
-# win_index_7 = win_combo7[6]
-# win_index_8 = win_combo8[7]
-
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 def won?(board)
+  win_index_1 = win_combo1[0]
+  win_index_2 = win_combo2[1]
+  win_index_3 = win_combo3[2]
+  win_index_4 = win_combo4[3]
+  win_index_5 = win_combo5[4]
+  win_index_6 = win_combo6[5]
+  win_index_7 = win_combo7[6]
+  win_index_8 = win_combo8[7]
+  
   WIN_COMBINATIONS.each do |index|
     if board[win_combo1] == " " " " " " && board[win_combo2] == " " " " " " && board[win_combo3] == " " " " " "
       true
